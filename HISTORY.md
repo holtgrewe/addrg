8f251741164449fc92be7b3a75890224920cc5f0
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2017-03-13 16:23:19 +0100

    Adding possibility to add platform

---
+++ HISTORY.md
@@ -0,0 +1,9 @@
+# History/Changelog
+
+## v0.2
+
+- Also adding sequencing platform
+
+## v0.1
+
+- Initial release
--- addrg.c
+++ addrg.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     samFile * in, * of;
 
     if(argc < 3 || strcmp(argv[1], "-h") == 0) {
-        printf("Usage: %s file.bam rg-name [sm-name]\n", argv[0]);
+        printf("Usage: %s file.bam rg-name [sm-name=rg-name] [platform=ILLUMINA]\n", argv[0]);
         printf("       e.g. %s some.bam rg-id\n", argv[0]);
         printf("            %s some.bam rg-id sm-name\n", argv[0]);
         printf("adds a read-group to a bam writes to stdout\n");
@@ -20,18 +20,21 @@ int main(int argc, char *argv[]) {
 
     char *rg = argv[2];
     char *sm = argv[3];
-    if (argc < 4)
+    if (argc >= 4)
         sm = rg;
+    char *pl = "ILLUMINA";
+    if (argc >= 5)
+        pl = argv[4];
 
     in = strcmp(argv[1], "-")? sam_open(argv[1], "rb") : sam_open("/dev/stdin", "rb");
 
     header = sam_hdr_read(in);
     read = bam_init1();
-    char *fmt = "@RG\tID:%s\tSM:%s";
+    char *fmt = "@RG\tID:%s\tSM:%s\tPL:%s";
 
     of = sam_open("/dev/stdout", "wb");
-    char *rg_full = malloc(sizeof(char) * (2 * strlen(rg) + strlen(fmt) - 2));
-    int rg_len = sprintf(rg_full, fmt, rg, sm);
+    char *rg_full = malloc(sizeof(char) * (strlen(rg) + strlen(sm) + strlen(pl) + strlen(fmt)));
+    int rg_len = sprintf(rg_full, fmt, rg, sm, pl);
 
     header->text = realloc(header->text, strlen(header->text) + rg_len);
     header->l_text = sprintf(header->text, "%s%s\n", header->text, rg_full);
